cf334a65253612fcce729f300684aff39cf6c95c
Daniel Muller <mrmeku@gmail.com>
2015-03-20 09:39:20 -0400

    cleaned up code a bit

--- interview-guide/js/main.js
+++ interview-guide/js/main.js
@@ -1,9 +1,67 @@
 $(document).ready(function() {
+    'use strict';
+
+    function buildField(field) {
+        var title = field.title || '';
+        var value = field.value || '';
+
+        var $field = $('<div>').addClass('intake-field');
+        $field.append(
+            $('<span>').addClass('name').text(title)
+        );
+        $field.append(
+            $('<span>').addClass('value').text(value)
+        );
+        return $field;
+    }
+
+    function buildSection(section) {
+        var title = section.title || '';
+        var fields = section.fields || [];
+
+        var $section = $('<div>').addClass('intake-section');
+        $section.append(
+            $('<header>').addClass('title').text(title)
+        );
+
+        for (var i=0; i < fields.length; i++) {
+            var $field = buildField(fields[i]);
+            $section.append($field);
+        }
+
+        return $section;
+    }
+
+    function buildInfoHtml(data) {
+        var sections = data.sections || [];
+        var $container = $('#intake-information');
+
+        for (var i=0; i < sections.length; i++) {
+            var $section = buildSection(sections[i]);
+            $container.append($section);
+        }
+    }
+
+    function loadIntakeInfo() {
+        var filePath = 'interview-dummy.json';
+        $.ajax({
+            url: filePath,
+            dataType: 'json',
+            method: 'GET',
+            success: buildInfoHtml,
+            error: function() {
+                console.log('error, intake info not built');
+            }
+        });
+    }
+
     $('.scroll-button.up').click(function() {
-        $(this).parent()[0].scrollTop -= 200;
+        $(this).parent()[0].scrollTop -= 187;
     });
 
     $('.scroll-button.down').click(function() {
-        $(this).parent()[0].scrollTop += 200;
+        $(this).parent()[0].scrollTop += 187;
     });
+
+    loadIntakeInfo();
 });
