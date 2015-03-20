a512fd31cceef1cbc991c06a92a455a231c47650
Leon Zhang <lwzhang@andrew.cmu.edu>
2015-03-19 20:14:19 -0400

    added intake info json loader (don't forget to run chrome with --allow-access-from-files flag)

--- interview-guide/js/loadIntakeInfo.js
+++ interview-guide/js/loadIntakeInfo.js
@@ -1,12 +1,41 @@
+function buildField(field) {
+    var title = field.title || '';
+    var value = field.value || '';
+    
+    var $field = $('<div>').addClass('intake-field');
+    $field.append(
+        $('<span>').addClass('field-title').text(title)
+    );
+    $field.append(
+        $('<span>').addClass('field-value').text(value)
+    );
+    return $field;
+}
+
 function buildSection(section) {
     var title = section.title || '';
     var fields = section.fields || [];
+
+    var $section = $('<div>').addClass('intake-section');
+    $section.append(
+        $('<header>').addClass('intake-title').text(title)
+    );
+
+    for (var i=0; i < fields.length; i++) {
+        var $field = buildField(fields[i]);
+        $section.append($field);
+    }
+
+    return $section;
 }
 
 function buildInfoHtml(data) {
-    var sections = data.sections;
+    var sections = data.sections || [];
+    var $container = $('#intake-information');
+
     for (var i=0; i < sections.length; i++) {
-        buildSection(sections[i]);
+        var $section = buildSection(sections[i]);
+        $container.append($section);
     }
 }
 
