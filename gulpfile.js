1952294a7925c2d583f654bbc7beb88f8c50279a
Daniel Muller <mrmeku@gmail.com>
2015-04-19 11:23:25 -0400

    Cleaned up code which generates intake-forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,44 +5,36 @@ var gulp = require('gulp'),
     intakeFormGenerator = require('./intake-form-generator.js');
 
 var intakeQuestionsDir= './intake_questions/';
-var intakeFormSkeletonPath = 'intake-form-skeleton.html';
+var intakeFormSkeletonPath = './intake-form-skeleton.html';
 
 gulp.task('default', function() {
-  console.log('TODO: write useful info to console');
+    console.log('gulp update-intake-forms: Generates intake forms from questions in intake_questions');
 });
 
 gulp.task('update-intake-forms', function() {
-  // Get all the jsons with questions used to generate intake-forms
-  var intakeFormSkeleton = fs.readFileSync(intakeFormSkeletonPath);
-  fs.readdir(intakeQuestionsDir, function(err, files) {
-    if (err) return console.error(err);
-    files.forEach(function(fileName) {
-      var filePath = intakeQuestionsDir + fileName,
-          animalName = fileName.slice(0, fileName.lastIndexOf('.json')),
-          outputFile = 'www/intake-form-' + animalName + '.html';
-      if (filePath.indexOf('.json') === -1) {
-        return;
-      }
+    // Get all the jsons with questions used to generate intake-forms
+    var intakeFormSkeleton = fs.readFileSync(intakeFormSkeletonPath);
+    var files = fs.readdirSync(intakeQuestionsDir);
 
-      fs.readFile(filePath, 'utf8', function(err, data) {
-        if (err) return console.error(err);
-        var questions = JSON.parse(data),
-            document = jsdom(intakeFormSkeleton),
+    files.forEach(function(fileName) {
+        if (fileName.indexOf('.json') === -1) { return; }
+
+        var questionsPath = intakeQuestionsDir + fileName,
+            questions = JSON.parse(fs.readFileSync(questionsPath, 'utf8'));
+
+        var animalName = fileName.slice(0, fileName.lastIndexOf('.json')),
+            outputFile = 'www/intake-form-' + animalName + '.html';
+
+        var document = jsdom(intakeFormSkeleton),
             $ = jquery(document.parentWindow),
             $body = $(document.body);
+
+        var formTitle = animalName.charAt(0).toUpperCase() + animalName.slice(1) + ' Surrender Form',
             formContent = intakeFormGenerator.generateFormContent($, questions);
 
-        var title = animalName.charAt(0).toUpperCase() + animalName.slice(1) + ' Surrender Form';
-        $body.find('#title').text(title);
+        $body.find('#title').text(formTitle);
         $body.find('#dynamic-content').append(formContent);
 
-        fs.writeFile(outputFile, document.documentElement.innerHTML, function(err) {
-          if(err) {
-              return console.error(err);
-          }
-          console.log("Created file: " + outputFile);
-        });
-      });
+        return fs.writeFileSync(outputFile, document.documentElement.innerHTML);
     });
-  });
 });
