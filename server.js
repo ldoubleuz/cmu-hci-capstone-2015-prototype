c52cdb31da417b1373999efbb0f30438c3953f23
Leon Zhang <lwzhang@andrew.cmu.edu>
2015-04-20 11:51:26 -0400

    abstract google calendar oauth flow; update readme with server instructions

--- server.js
+++ server.js
@@ -19,13 +19,16 @@ db.once('open', function (callback) {
   console.log('MongoDB connection open at ' + mongooseUrl);
 });
 
-
 // Google calendar authentication info
 var googleAPIKeys = require('./googleAPIKeys');
-var calendar = google.calendar('v3');
+var googleCalendar = google.calendar('v3');
     oAuthClient = null, // To be setup by _initGoogleClient
-    authedCalendar = false,
-    OAUTH_REDIRECT_PATH = '/admin/auth-calendar';
+    calendarIsAuthed = false,
+    // what path the oauth request will automatically redirect to
+    // (should match url set up in the Google API client)
+    OAUTH_REDIRECT_PATH = '/admin/auth-calendar',
+    // what path we should redirect to after a successful admin calendar oatuh
+    OAUTH_SUCCESS_PATH = '/calendar-demo';
 
 function _initGoogleClient() {
   var host = 'localhost';
@@ -40,6 +43,34 @@ function _initGoogleClient() {
   console.log("google client initialized pointing at %s", redirectURL);
 };
 
+// TODO: setup sessions for a site admin, then call this before anything that
+// should require an admin login
+function shouldTriggerAuthAdmin(req, res) {
+  return !(req.session && req.session.adminAuthed);
+}
+
+function doAuthAdmin(req, res) {
+  if (shouldTriggerAuthAdmin(req, res)) {
+    // TODO: do stuff to login a user
+    throw new Exception("site admin auth not yet implemented");
+  }
+}
+
+function shouldTriggerAuthCalendar(req, res) {
+  return !calendarIsAuthed;
+}
+
+function doAuthCalendar(req, res) {
+  if (shouldTriggerAuthCalendar(req, res)) {
+    // trigger oauth flow
+    // Generate an OAuth URL and redirect there
+    var url = oAuthClient.generateAuthUrl({
+      access_type: 'offline', // allows us to get a refresh token
+      scope: 'https://www.googleapis.com/auth/calendar' // use write scope
+    });
+    res.redirect(url);
+  }
+}
 
 // How we plan to store info from the intake form
 var intake = mongoose.model('intake', {
@@ -50,21 +81,16 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/calendar-demo', function(req, res) {
-  if (!authedCalendar) {
+  if (shouldTriggerAuthCalendar(req, res)) {
     // trigger oauth flow
-    // Generate an OAuth URL and redirect there
-    var url = oAuthClient.generateAuthUrl({
-      access_type: 'offline',
-      scope: 'https://www.googleapis.com/auth/calendar' // use write scope
-    });
-    res.redirect(url);
+    doAuthCalendar(req, res);
   } else {
     // Stolen from http://www.matt-toigo.com/dev/pulling_google_calendar_events_with_node
     // Format today's date
     var today = moment().format('YYYY-MM-DD') + 'T';
 
     // Call google to fetch events for today on our calendar
-    calendar.events.list({
+    googleCalendar.events.list({
       calendarId: googleAPIKeys.calendarID,
       maxResults: 20,
       timeMin: today + '00:00:00.000Z',
@@ -97,12 +123,17 @@ app.get(OAUTH_REDIRECT_PATH, function(req, res) {
         console.log('Successfully authenticated');
         console.log(tokens);
         
-        // Store our credentials and redirect back to our main page
+        // Store our credentials and redirect back to some page
         oAuthClient.setCredentials(tokens);
-        authedCalendar = true;
-        res.redirect('/calendar-demo');
+        calendarIsAuthed = true;
+        res.redirect(OAUTH_SUCCESS_PATH);
       }
     });
+  } else if (shouldTriggerAuthCalendar(req, res)) {
+    doAuthCalendar(req, res);
+  } else {
+    // if already authenticated, skip oauth workflow
+    res.redirect(OAUTH_SUCCESS_PATH);
   }
 });
 
